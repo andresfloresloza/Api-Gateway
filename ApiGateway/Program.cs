7cd6e09b5d701b92c901345ce57622b436288d9e
andresfloresloza <luisandresfloresloza@gmail.com>
2022-11-21 00:28:42 -0400

    Implementacion Microservicio Compras

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,51 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("ocelot.json");
+
+var secretKey = builder.Configuration.GetValue<string>("JwtOptions: SecretKey");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
+            ClockSkew = new System.TimeSpan(0)
+        };
+    });
+
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers(); 
+app.UseOcelot().Wait();
+
+
+app.Run();
